3a7f5c01be5ef125f7fcc4111bf7658b290afe2e
Deivs <deividi_henrique@hotmail.com>
2021-08-03 16:21:57 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function openNav(){
+    document.getElementById("minhaBarra").style.width = "250px";
+    document.getElementById("menu").style.marginLeft = "250px";
+}
+
+function closeNav(){
+    document.getElementById("minhaBarra").style.width = "0";
+    document.getElementById("menu").style.width = "0";
+    document.getElementById("menu").style.marginLeft = "10px";
+}
